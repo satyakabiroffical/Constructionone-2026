9c55ac7eede9f9c64192409f5365ecbc7d9445bb
asgardev119 <asgar.ies.bpl@gmail.com>
2026-02-17 10:14:51 +0530

    user address

--- src/controllers/user/cart.controller.js
+++ src/controllers/user/cart.controller.js
@@ -91,5 +91,4 @@ export const removeCartItem = async (req, res, next) => {
     next(error);
   }
 };
-
 //asgr
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import address from "./address.routes.js";
 import cart from "./cart.routes.js";
+import wishlist from "./wishlist.routes.js";
 const router = Router();
 
 router.use("/address", address);
 router.use("/cart", cart);
+router.use("/wishlist", wishlist);
 
 export default router;
 
